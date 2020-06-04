2fb53a423e5823b4cdfbb61009f69affab7de8bf
Barbara <6abi.cardoso@gmail.com>
2020-06-04 06:05:13 -0300

    versão final de controle

--- stopHere_firebaseVersion/smartparking/smartparking.ino
+++ stopHere_firebaseVersion/smartparking/smartparking.ino
@@ -7,47 +7,54 @@
 #include <Servo.h>
 
 
-const char *ssid =  "unsafe";           // Enter your WiFi Name
-const char *pass =  "Lem@nn91";        // Enter your WiFi Password
+const char *ssid =  "unsafe";                        // Enter your WiFi Name
+const char *pass =  "Lem@nn91";                     // Enter your WiFi Password
 
-Servo myservoEntry;                   //servo as gate
-Servo myservoExit;                   //servo as gate
+Servo myservoEntry;                               //servo as gate
+Servo myservoExit;                               //servo as gate
 
-int carEnter = D0;                    // entry sensor 
-int carExited = D2;                  //exi sensor
-int slot4 = D8;                 
-int slot3 = D7;                 
-int slot2 = D6;
-int slot1  = D5;
- 
-const int CLOSE_ANGLE = 90;              // The closing angle of the servo motor arm
-const int OPEN_ANGLE = 0;               // The opening angle of the servo motor arm    
-          
-int  hh, mm, ss;
-int count = 0;
+int carEnter = D0;                              // entry sensor 
+int carExited = A0;                           //exi sensor - LDR analogRead
+int led = D6;                                //LED exit gate
+
+
+
+const int CLOSE_ANGLE = 90;                 // The closing angle of the servo motor arm
+const int OPEN_ANGLE = 0;                  // The opening angle of the servo motor arm    
+
+
+//slots sensors - IR
+int slot6 = D8;                            //special slot                
+int slot4 = D1;                 
+int slot2 = D5;
+int slot1  = D7;
+
+//time of use for calculate the value to pay
+int  hh, mm, ss;                         //Set the time of use
 int pos;
 int posTwo;
-const int total = 3;
-int space;
+int total = 6;
+
+
+int slotAvailable = 0;
 
 //for synchronized control with the front
 int flag1 = 0;
 int flag2 = 0;
-int special_slot = 1;
+int specialSlot = 1;
+int slotToGo;
 
 //String h, m,EntryTimeSlot1,ExitTimeSlot1, EntryTimeSlot2,ExitTimeSlot2, EntryTimeSlot3,ExitTimeSlot3, EntryTimeSlot4,ExitTimeSlot4;
-boolean entrysensor, exitsensor;
+boolean entrysensor;
+int exitsensor = 0;       //LDR's value reed
 boolean s1,s2,s3,s4,s5,s6;
 
-boolean s1_occupied = false;
-boolean s2_occupied = false;
-boolean s3_occupied = false;
-boolean s4_occupied = false;
-boolean s5_occupied = true;
-boolean s6_occupied = true;
-
-
-
+int s1_occupied = 1;
+int s2_occupied = 1;
+int s3_occupied = 1;
+int s4_occupied = 1;
+int s5_occupied = 1;
+int s6_occupied = 1;
 
 //void slotOne();
 //void slotTwo();
@@ -59,29 +66,39 @@ boolean s6_occupied = true;
 
 void setup() {
   delay(1000);
-  Serial.begin (9600); 
+  Serial.begin (115200); 
 
   myservoEntry.attach(D3);             // servo pin to D3
   myservoExit.attach(D4);            // servo pin to D5
   pinMode(carExited, INPUT);        // ir as input
   pinMode(carEnter, INPUT);         // ir as input
+  pinMode(led,OUTPUT);
+
   pinMode(slot1, INPUT);
   pinMode(slot2, INPUT);
-  pinMode(slot3, INPUT);
   pinMode(slot4, INPUT);
+  pinMode(slot6, INPUT);
   wifiConnection();
 }
 
 void loop() {
   entryGate();
+  slotDirection();
   exitGate();
   slotOne();
   slotTwo();
   slotThree();
-  delay(3000);
-  Serial.print("total de vagas disponives: ");
-  Serial.println(space);
+  slotFour();
+  slotFive();
+  slotSix();
+  countSlots();
 
+
+  Serial.print("total de vagas: ");
+  Serial.println(total);
+  Serial.print("total de vagas diponíveis: ");
+  Serial.println(slotAvailable);
+  delay(3000);
 }
 
 void wifiConnection(){
@@ -97,18 +114,18 @@ void wifiConnection(){
     Serial.println(ssid);
     Serial.print("IP Address is : ");
     Serial.println(WiFi.localIP());                        //print local IP address
+    Serial.print(" MAC Address is : ");
+    Serial.println(WiFi.macAddress());
  }
 void entryGate(){
-   space = total;
    entrysensor= !digitalRead(carEnter);
     if (entrysensor == 1) {                             // if high then count and send data
-      if(space > 0 ){
-         count=  count+1;                             //increment count
-         myservoEntry.write(OPEN_ANGLE);
-         delay(3000);
+      if(slotAvailable > 0){
+        Serial.print("Dirija-se até a vaga número: ");
+        Serial.println(slotToGo);
          myservoEntry.write(CLOSE_ANGLE);
-         space = space -1;
-      }
+         delay(3000);
+         myservoEntry.write(OPEN_ANGLE);      }
       else{
         Serial.println("Desculpe, nenuhuma vaga disponível");
         delay(1000);
@@ -117,92 +134,157 @@ void entryGate(){
 }
 
 void exitGate(){
-   exitsensor = !digitalRead(carExited);
-   if (exitsensor == 1) {                                 //if high then count and send
-     count= count-1;                                    //decrement count
+   exitsensor = analogRead(carExited);
+   digitalWrite(led, LOW);
+   if (exitsensor < 1020) {                                 //if high then count and send
+     digitalWrite(led, HIGH);
      myservoExit.write(OPEN_ANGLE);
+     Serial.println("Cancela Aberta");
      delay(3000);
      myservoExit.write(CLOSE_ANGLE);
-     space = space +1;
   }
+  else{
+    digitalWrite(led, LOW);
+    Serial.println("Cancela Fechada");
+  }
+  delay(10);
  }
- 
-void slotOne(){
-  s1 = digitalRead(slot1);
-  if (s1 == 1 && s1_occupied == false) {                     
+
+ void slotOne(){
+  s1 = !digitalRead(slot1);
+  if (s1 == 1 && s1_occupied == 1) {                     
         Serial.println("vaga 1 está disponivel ");
-        s1_occupied = true;
-   }
-  if(s1 == 0 && s1_occupied == true) {
-       Serial.println("Vaga 1 está ocupada ");
-       s1_occupied = false;
+        s1_occupied = 0;
+        
   }
-  
+ if (s1 == 1 && s1_occupied == 0) {
+     Serial.println(" Vaga 1 está ocupada ");
+     s1_occupied = 0;
+  }
+  if(s1 == 0 && (s1_occupied == 1 || s1_occupied == 0 )){
+      Serial.println("Vaga 1 está disponivel "); 
+      s1_occupied = 1;
+  }
 }
 
 void slotTwo(){
-  s2 = digitalRead(slot2);
-  if (s2 == 1&& s2_occupied == false) {                     
+  s2 = !digitalRead(slot2);
+  if (s2 == 1&& s2_occupied == 1) {                     
       Serial.println("Vaga 2 está disponivel "); 
-      s2_occupied = true;
-    }
- if (s2 == 0 && s2_occupied == true) {
+      s2_occupied = 0;
+  }
+ if (s2 == 1 && s2_occupied == 0) {
      Serial.println(" Vaga 2 está ocupada ");
-
-     s2_occupied = false;
+     s2_occupied = 0;
   }
-  
+  if(s2 == 0 && (s2_occupied == 1 ||s2_occupied == 0 )){
+      Serial.println("Vaga 2 está disponivel "); 
+      s2_occupied = 1;
+  }
 }
 
-//special slot
 void slotThree(){
-  s3 = digitalRead(slot3);
-  if (s3 == 1&& s3_occupied == false) {                     
-     Serial.println("Vaga 3 - preferencial, está disponivel ");
-     s3_occupied = true;
-     special_slot = 1;
-     
+  s3 = 1;
+  if (s3 == 1 && s3_occupied == 1) {                     
+        Serial.println("vaga 3 está disponivel ");
+        s3_occupied = 0;
+        
   }
- if(s3 == 0 && s3_occupied == true) {
-     Serial.println("Vaga 3 - preferencial, ocupada ");
-     s3_occupied = false;
-     special_slot = special_slot - 1;
- } 
+ if (s3 == 1 && s3_occupied == 0) {
+     Serial.println(" Vaga 3 está ocupada ");
+     s3_occupied = 0;
+  }
+  if(s3 == 0 && (s3_occupied == 1 || s3_occupied == 0 )){
+      Serial.println("Vaga 3 está disponivel "); 
+      s3_occupied = 1;
+  }
 }
 
-void slotFower(){
-  s4 = digitalRead(slot4);
-  if (s4 == 1&& s4_occupied == false) {                     
-     Serial.println("Vaga 4 está disponivel ");
-     s4_occupied = true;
+ void slotFour(){
+  s4 = !digitalRead(slot4);
+  if (s4 == 1 && s4_occupied == 1) {                     
+        Serial.println("vaga 4 está disponivel ");
+        s4_occupied = 0;
+        
   }
- if(s4 == 0 && s4_occupied == true) {
-     Serial.println("Vaga 4 ocupada ");
-     s4_occupied = false;
- } 
+ if (s4 == 1 && s4_occupied == 0) {
+     Serial.println(" Vaga 4 está ocupada ");
+     s4_occupied = 0;
+  }
+  if(s4 == 0 && (s4_occupied == 1 || s4_occupied == 0 )){
+      Serial.println("Vaga 4 está disponivel "); 
+      s4_occupied = 1;
+  }
 }
 
+//vague ghost only for demonstration
 void slotFive(){
-  s5 = 0;
-  if (s3 == 1&& s3_occupied == false) {                     
-     Serial.println("Vaga 5 está disponivel ");
-     s3_occupied = true;
+  s5 = 1;
+  if (s5 == 1 && s5_occupied == 1) {                     
+        Serial.println("vaga 5 está disponivel ");
+        s5_occupied = 0;
+        
   }
- if(s3 == 0 && s3_occupied == true) {
-     Serial.println("Vaga 5 ocupada ");
-     s3_occupied = false;
- } 
+ if (s5 == 1 && s5_occupied == 0) {
+     Serial.println(" Vaga 5 está ocupada ");
+     s5_occupied = 0;
+  }
+  if(s5 == 0 && (s5_occupied == 1 || s5_occupied == 0 )){
+      Serial.println("Vaga 5 está disponivel "); 
+      s5_occupied = 1;
+  }
 }
 
 void slotSix(){
-  s6 = 0;
-  if (s3 == 1&& s3_occupied == false) {                     
-     Serial.println("Vaga 6 está disponivel ");
-     s3_occupied = true;
+  s6 = !digitalRead(slot6);
+  if (s6 == 1&& s6_occupied == 1) {                     
+      Serial.println("Vaga 6 está disponivel "); 
+      s6_occupied = 0;
   }
- if(s3 == 0 && s3_occupied == true) {
-     Serial.println("Vaga 6 ocupada ");
-     s3_occupied = false;
- } 
+ if (s6 == 1 && s6_occupied == 0) {
+     Serial.println(" Vaga 6 está ocupada ");
+     s6_occupied = 0;
+  }
+  if(s6 == 0 && (s6_occupied == 1 ||s6_occupied == 0 )){
+      Serial.println("Vaga 6 está disponivel "); 
+      s6_occupied = 1;
+  }
 }
+
+void countSlots(){
+  int count = 0;
+  for ( int i = 0; i < 2; i++){
+    count = (s1_occupied + s2_occupied + s3_occupied + s4_occupied + s5_occupied + s6_occupied);
+  }
+  slotAvailable = count;
+}
+
+void slotDirection(){
+  int slotDirection;
+  for (int i = 0; i < 2; i++){
+      slotDirection = 0;
+      if (s1_occupied == 1){
+      slotDirection = 1;
+      }
+      else if (s2_occupied == 1 && s1_occupied == 0){
+          slotDirection = 2;
+      }
+      else if (s3_occupied == 1 && s2_occupied == 0 && s1_occupied == 0){
+          slotDirection =3;
+      }
+      else if (s4_occupied == 1 && s3_occupied == 0 && s2_occupied == 0 && s1_occupied == 0){
+          slotDirection =4;
+      }
+      else if (s5_occupied == 1 && s4_occupied == 0 && s3_occupied == 0 && s2_occupied == 0 && s1_occupied == 0){
+          slotDirection =5;
+      }
+      else if (s6_occupied == 1 && s5_occupied == 0 && s4_occupied == 0 && s3_occupied == 0 && s2_occupied == 0 && s1_occupied == 0){
+          slotDirection =6;
+      }
+  }
+  slotToGo = slotDirection;
+}
+
+ 
+
  
